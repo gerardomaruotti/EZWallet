19bdd7141807332a7f3f06544a89e11fefc89573
GERARDO MARUOTTI s317642 <s317642@studenti.polito.it>
2023-05-17 19:04:14 +0200

    delete user

--- code/controllers/users.js
+++ code/controllers/users.js
@@ -1,6 +1,6 @@
-import { Group, User } from "../models/User.js";
-import { transactions } from "../models/model.js";
-import { verifyAuth } from "./utils.js";
+import { Group, User } from '../models/User.js';
+import { transactions } from '../models/model.js';
+import { verifyAuth } from './utils.js';
 
 /**
  * Return all the users
@@ -10,13 +10,13 @@ import { verifyAuth } from "./utils.js";
     - empty array is returned if there are no users
  */
 export const getUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-}
+	try {
+		const users = await User.find();
+		res.status(200).json(users);
+	} catch (error) {
+		res.status(500).json(error.message);
+	}
+};
 
 /**
  * Return information of a specific user
@@ -26,20 +26,21 @@ export const getUsers = async (req, res) => {
     - error 401 is returned if the user is not found in the system
  */
 export const getUser = async (req, res) => {
-    try {
-        const cookie = req.cookies
-        if (!cookie.accessToken || !cookie.refreshToken) {
-            return res.status(401).json({ message: "Unauthorized" }) // unauthorized
-        }
-        const username = req.params.username
-        const user = await User.findOne({ refreshToken: cookie.refreshToken })
-        if (!user) return res.status(401).json({ message: "User not found" })
-        if (user.username !== username) return res.status(401).json({ message: "Unauthorized" })
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json(error.message)
-    }
-}
+	try {
+		const cookie = req.cookies;
+		if (!cookie.accessToken || !cookie.refreshToken) {
+			return res.status(401).json({ message: 'Unauthorized' }); // unauthorized
+		}
+		const username = req.params.username;
+		const user = await User.findOne({ refreshToken: cookie.refreshToken });
+		if (!user) return res.status(401).json({ message: 'User not found' });
+		if (user.username !== username)
+			return res.status(401).json({ message: 'Unauthorized' });
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json(error.message);
+	}
+};
 
 /**
  * Create a new group
@@ -53,44 +54,53 @@ export const getUser = async (req, res) => {
     - error 401 is returned if all the `memberEmails` either do not exist or are already in a group
  */
 export const createGroup = async (req, res) => {
-    try {
-      const cookie = req.cookies
-      if (!cookie.accessToken) {
-          return res.status(401).json({ message: "Unauthorized" }) //unauthorized
-      }
-      const {name, memberEmails} = req.body;
-      const alreadyInGroup = [];
-      const membersNotFound = [];
+	try {
+		const cookie = req.cookies;
+		if (!cookie.accessToken) {
+			return res.status(401).json({ message: 'Unauthorized' }); //unauthorized
+		}
+		const { name, memberEmails } = req.body;
+		const alreadyInGroup = [];
+		const membersNotFound = [];
 
-      if (await Group.findOne({ name: name})) 
-        return res.status(401).json({ message: "A group with the same name already exists"});
+		if (await Group.findOne({ name: name }))
+			return res
+				.status(401)
+				.json({ message: 'A group with the same name already exists' });
 
-      memberEmails.forEach(async (e) => {
-        if (!(await User.findOne({ email: e}))) {
-          membersNotFound.append(e);
-        } 
-      });
-      memberEmails = memberEmails.filter((email) => !(email in membersNotFound));
+		memberEmails.forEach(async (e) => {
+			if (!(await User.findOne({ email: e }))) {
+				membersNotFound.append(e);
+			}
+		});
+		memberEmails = memberEmails.filter((email) => !(email in membersNotFound));
 
-      memberEmails.forEach(async (e) => {
-        if(await Group.findOne({ members: { email: e}})) { //FUNZIONE DA IMPLEMENTARE
-          alreadyInGroup.append(e);
-        }
-      });
-      memberEmails = memberEmails.filter((email) => !(email in alreadyInGroup));
+		memberEmails.forEach(async (e) => {
+			if (await Group.findOne({ members: { email: e } })) {
+				//FUNZIONE DA IMPLEMENTARE
+				alreadyInGroup.append(e);
+			}
+		});
+		memberEmails = memberEmails.filter((email) => !(email in alreadyInGroup));
 
-      if(memberEmails.length > 0)
-        return res.status(401).json({ message: "All th emails are invalid"});
-      
-      if (group) return res.status(401).json({ message: "a group with the same name already exists"})
-      const new_group = new Group({name, memberEmails});
-      new_group.save()
-          .then(data => res.json(data))
-          .catch(err => { throw err })
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+		if (memberEmails.length > 0)
+			return res.status(401).json({ message: 'All th emails are invalid' });
+
+		if (group)
+			return res
+				.status(401)
+				.json({ message: 'a group with the same name already exists' });
+		const new_group = new Group({ name, memberEmails });
+		new_group
+			.save()
+			.then((data) => res.json(data))
+			.catch((err) => {
+				throw err;
+			});
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Return all the groups
@@ -101,11 +111,11 @@ export const createGroup = async (req, res) => {
     - empty array is returned if there are no groups
  */
 export const getGroups = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Return information of a specific group
@@ -116,11 +126,11 @@ export const getGroups = async (req, res) => {
     - error 401 is returned if the group does not exist
  */
 export const getGroup = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Add new members to a group
@@ -134,11 +144,11 @@ export const getGroup = async (req, res) => {
     - error 401 is returned if all the `memberEmails` either do not exist or are already in a group
  */
 export const addToGroup = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Remove members from a group
@@ -151,11 +161,11 @@ export const addToGroup = async (req, res) => {
     - error 401 is returned if all the `memberEmails` either do not exist or are not in the group
  */
 export const removeFromGroup = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Delete a user
@@ -167,11 +177,40 @@ export const removeFromGroup = async (req, res) => {
     - error 401 is returned if the user does not exist 
  */
 export const deleteUser = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+		const email = req.body.email;
+		console.log(email);
+		const user = await User.findOne({ email: email });
+		console.log(user);
+		const findTransactions = await transactions.find({
+			username: user.username,
+		});
+		console.log(findTransactions);
+		const deletedTransactions = await transactions.deleteMany({
+			username: user.username,
+		});
+		const userGroup = await Group.find();
+		const emailExists = false;
+		userGroup.forEach((group) => {
+			group.members.forEach((member) => {
+				if (member.email === email) {
+					//remove member from group
+					emailExists = true;
+				}
+			});
+		});
+		console.log(userGroup);
+		console.log(emailExists);
+		// const deleteUser = await User.deleteMany({
+		// 	email: email,
+		// });
+		// const deletedTransactions = await transactions.deleteMany({
+		// 	email: email,
+		// });
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
 
 /**
  * Delete a group
@@ -181,8 +220,8 @@ export const deleteUser = async (req, res) => {
     - error 401 is returned if the group does not exist
  */
 export const deleteGroup = async (req, res) => {
-    try {
-    } catch (err) {
-        res.status(500).json(err.message)
-    }
-}
+	try {
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+};
