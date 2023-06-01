89a0f6a4c628e22bf4c8284a81611a78ca7a499d
Paolo Magliano s314867 <s314867@studenti.polito.it>
2023-06-01 17:14:39 +0200

    Done firsts test functions

--- code/test/users.unit.test.js
+++ code/test/users.unit.test.js
@@ -1,6 +1,18 @@
 import request from 'supertest';
 import { app } from '../app';
 import { User } from '../models/User.js';
+import {
+	getUsers,
+	getUser,
+	createGroup,
+	getGroups,
+	deleteGroup,
+	getGroup,
+	deleteUser,
+	addToGroup,
+	removeFromGroup,
+} from '../controllers/users.js';
+import { verifyAuth } from '../controllers/utils';
 
 /**
  * In order to correctly mock the calls to external modules it is necessary to mock them using the following line.
@@ -10,6 +22,9 @@ import { User } from '../models/User.js';
  */
 jest.mock('../models/User.js');
 
+jest.mock('../controllers/utils');
+
+
 /**
  * Defines code to be executed before each test case is launched
  * In this case the mock implementation of `User.find()` is cleared, allowing the definition of a new mock implementation.
@@ -17,38 +32,75 @@ jest.mock('../models/User.js');
  */
 beforeEach(() => {
 	User.find.mockClear();
+	verifyAuth.mockClear();
+
 	//additional `mockClear()` must be placed here
 });
 
 describe('getUsers', () => {
+	test('should return 401 if user is not logged in', async () => {
+		const mockReq = {
+			cookies: {},
+			body: {},
+			params: {}
+		};
+		const mockRes = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn()
+		};
+
+		verifyAuth.mockImplementation(() => ({ authorized: false, cause: 'No cookies' }));
+
+		await getUsers(mockReq, mockRes);
+		
+		expect(mockRes.status).toHaveBeenCalledWith(401);
+		expect(mockRes.json).toHaveBeenCalledWith({ error: 'No cookies' });
+	});
+
+
+
 	test('should return empty list if there are no users', async () => {
-		//any time the `User.find()` method is called jest will replace its actual implementation with the one defined below
+		const mockReq = {
+			cookies: {},
+			body: {},
+			params: {}
+		};
+		const mockRes = {
+			status: jest.fn().mockReturnThis(),
+			json: jest.fn(),
+			locals: {
+				refreshedTokenMessage: 'Refreshed token'
+			}
+		};
+
 		jest.spyOn(User, 'find').mockImplementation(() => []);
-		const response = await request(app).get('/api/users');
+		verifyAuth.mockImplementation(() => ({ authorized: true }));
 
-		expect(response.status).toBe(200);
-		expect(response.body).toEqual([]);
+		await getUsers(mockReq, mockRes);
+
+		expect(mockRes.status).toHaveBeenCalledWith(200);
+		expect(mockRes.json).toHaveBeenCalledWith({ data: [], refreshedTokenMessage: 'Refreshed token'  });
 	});
 
-	test('should retrieve list of all users', async () => {
-		const retrievedUsers = [
-			{
-				username: 'test1',
-				email: 'test1@example.com',
-				password: 'hashedPassword1',
-			},
-			{
-				username: 'test2',
-				email: 'test2@example.com',
-				password: 'hashedPassword2',
-			},
-		];
-		jest.spyOn(User, 'find').mockImplementation(() => retrievedUsers);
-		const response = await request(app).get('/api/users');
+	// test('should retrieve list of all users', async () => {
+	// 	const retrievedUsers = [
+	// 		{
+	// 			username: 'test1',
+	// 			email: 'test1@example.com',
+	// 			password: 'hashedPassword1',
+	// 		},
+	// 		{
+	// 			username: 'test2',
+	// 			email: 'test2@example.com',
+	// 			password: 'hashedPassword2',
+	// 		},
+	// 	];
+	// 	jest.spyOn(User, 'find').mockImplementation(() => retrievedUsers);
+	// 	const response = await request(app).get('/api/users');
 
-		expect(response.status).toBe(200);
-		expect(response.body).toEqual(retrievedUsers);
-	});
+	// 	expect(response.status).toBe(200);
+	// 	expect(response.body).toEqual(retrievedUsers);
+	// });
 });
 
 describe('getUser', () => {});
--- code/test/utils.unit.test.js
+++ code/test/utils.unit.test.js
@@ -11,9 +11,21 @@ describe('handleDateFilterParams', () => {
 });
 
 describe('verifyAuth', () => {
-	test('Dummy test, change it', () => {
-		expect(true).toBe(true);
+	test('should return 401 if there are no cookies', () => {
+		const mockReq = {
+			cookies: {},
+			body: {},
+			params: {}
+		};
+		const mockRes = {};
+
+		const response = verifyAuth(mockReq, mockRes);
+
+		expect(response).toHaveProperty('authorized', false);
+		expect(response).toHaveProperty('cause');
 	});
+
+	test
 });
 
 describe('handleAmountFilterParams', () => {
